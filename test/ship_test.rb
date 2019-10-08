0603a48aee585f9fe79d431cf530995c0892cf93
Jesse Higgins <jessofjames@gmail.com>
2019-10-08 16:58:25 -0600

    Add name and length test.
    Co-authored-by: Jomah Fangonilo <53122061+jfangonilo@users.noreply.github.com>

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -10,4 +10,12 @@ class ShipTest < Minitest::Test
   def test_it_exists
     assert_instance_of Ship, @cruiser
   end
+
+  def test_it_has_a_name
+    assert_equal "Cruiser", @cruiser.name
+  end
+
+  def test_it_has_a_length
+    assert_equal 3, @cruiser.length
+  end
 end
