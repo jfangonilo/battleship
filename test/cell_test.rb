0c31748c60c376f514b8fb7f5df2e875a3adf8cf
Jesse Higgins <jessofjames@gmail.com>
2019-10-09 18:20:59 -0600

    Add ship visibility test to render test factoring to "S".
    Co-authored-by: Jomah Fangonilo <53122061+jfangonilo@users.noreply.github.com>

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -72,6 +72,8 @@ class CellTest < Minitest::Test
     @cell_1.fire_upon
     assert_equal "M", @cell_1.render
     @cell_2.place_ship(@cruiser)
+    assert_equal ".", @cell_2.render
+    assert_equal "S", @cell_2.render(true)
     @cruiser.hit
     assert_equal false, @cruiser.sunk?
     @cruiser.hit
