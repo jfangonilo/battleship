2ec902e510a81fe9413fe1619aba0c777407c904
Jesse Higgins <jessofjames@gmail.com>
2019-10-10 15:32:52 -0600

    Adds test_coordinate_numbers_equals_ship_length.
    Co-authored-by: Jomah Fangonilo <53122061+jfangonilo@users.noreply.github.com>

--- test/board_test.rb
+++ test/board_test.rb
@@ -7,6 +7,8 @@ require 'minitest/autorun'
 class CellTest < Minitest::Test
   def setup
     @board = Board.new
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
   end
 
   def test_it_exists
@@ -31,5 +33,10 @@ class CellTest < Minitest::Test
     assert_equal false, @board.valid_coordinate?("A5")
     assert_equal false, @board.valid_coordinate?("E1")
     assert_equal false, @board.valid_coordinate?("A22")
+  end
+
+  def test_coordinate_numbers_equals_ship_length
+    assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2"])
+    assert_equal false, @board.valid_placement?(@submarine, ["A2", "A3", "A4"])
   end
 end
