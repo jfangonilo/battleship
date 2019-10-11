ff2bd0f6053ec58d2c7b2f74d16cbeef35f1dc94
Jesse Higgins <jessofjames@gmail.com>
2019-10-11 03:08:55 -0600

    Adds functionality to valid_placement? method with case test. Redefines valid_placement? argument (coordinates) to (placement_coordinates). Adds setup method. Adds @placement_coordinates instance variable as range of 16 hash values converted to array in setup method. Adds range of cell numbers converted to array in setup method. Adds range of cell letters converted to array in setup method. Adds @cell_numbers and @cell_letters instance variables in setup method.
    Co-authored-by: Jomah Fangonilo <53122061+jfangonilo@users.noreply.github.com>

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,8 @@
 class Board
-  def cells
-    {
+  attr_reader :cells
+
+  def initialize
+    @cells = {
       "A1" => Cell.new("A1"),
       "A2" => Cell.new("A2"),
       "A3" => Cell.new("A3"),
@@ -20,12 +22,28 @@ class Board
     }
   end
 
+  def setup
+    @first_cell = @cells.to_a.first(0)
+    @last_cell = @cells.to_a.last(15)
+    #@placement_coordinates = @first_cell.first..@last_cell.last).to_a
+    @placement_coordinates = Range.new(@first_cell..@last_cell)
+    @cell_numbers = Range.new(1..4).to_a
+    @cell_letters = Range.new("A".."D".ord).to_a
+  end
+
   def valid_coordinate?(coordinate)
     cells.key?(coordinate)
   end
 
-  def valid_placement?(ship, coordinates)
-    return true if coordinates.length == ship.length
-    false
+  def valid_placement?(ship, placement_coordinates)
+    case
+    when @cell_numbers || @cell_letters
+      true
+    when placement_coordinates.each_cons(ship.length) { |cell| }
+      true
+    else
+      false
+    end
   end
+
 end
