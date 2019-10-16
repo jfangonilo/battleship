83d67514ddebb6f5a4211c1cdbc0d5fc8004daed
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-10-16 15:08:33 -0600

    Add additional valid placement tests

--- test/setup_test.rb
+++ test/setup_test.rb
@@ -21,28 +21,62 @@ class SetupTest < Minitest::Test
     assert_instance_of Board, @setup.player_board
   end
 
-  def test_it_can_get_player_input
-    skip
-    assert_equal ["A3", "A2", "A1"], @setup.input
-  end
-
   def test_it_can_validate_inputs
-    skip
-    assert_equal true, @setup.good_coordinates?(@setup.input)
+    assert_equal true, @setup.good_coordinates?(["A1", "A4"])
+    assert_equal false, @setup.good_coordinates?(["1B"])
+    assert_equal false, @setup.good_coordinates?(["BA"])
+    assert_equal false, @setup.good_coordinates?(["sgdkjbsd"])
+    assert_equal false, @setup.good_coordinates?(["1"])
+    assert_equal false, @setup.good_coordinates?(["B"])
   end
 
   def test_it_can_validate_placements
-    skip
-    assert_equal true, @setup.good_placement?(@submarine, @setup.input)
+    assert_equal true, @setup.good_placement?(@submarine, ["A1", "B1"])
+    assert_equal true, @setup.good_placement?(@submarine, ["C1", "B1"])
+    assert_equal false, @setup.good_placement?(@submarine, ["A1", "B2"])
+    assert_equal false, @setup.good_placement?(@submarine, ["A1", "A3"])
+    assert_equal false, @setup.good_placement?(@submarine, ["A1"])
+    assert_equal false, @setup.good_placement?(@submarine, ["sfhass"])
+    assert_equal false, @setup.good_placement?(@submarine, ["1B", "1C"])
+    assert_equal true, @setup.good_placement?(@cruiser, ["A1", "B1", "C1"])
+    assert_equal true, @setup.good_placement?(@cruiser, ["B3", "B2", "B4"])
+    assert_equal false, @setup.good_placement?(@cruiser, ["A1", "B2", "C3"])
+    assert_equal false, @setup.good_placement?(@cruiser, ["A1", "A3", "A4"])
+    assert_equal false, @setup.good_placement?(@cruiser, ["A1", "A2"])
+    assert_equal false, @setup.good_placement?(@cruiser, ["sfhass"])
+    assert_equal false, @setup.good_placement?(@cruiser, ["1B", "1C", "1A"])
   end
 
   def test_player_can_place
-
+    skip
     @setup.player_place
   end
 
   def test_computer_place
     skip
     @setup.computer_place(@cruiser)
+  end
+
+  def test_vertical
+    puts "\nAll sets should have like number coordinate"
+    p @setup.vertical_placement(["A", "B", "C", "D"], ["1", "2", "3", "4"], @cruiser)
+    p @setup.vertical_placement(["A", "B", "C", "D"], ["1", "2", "3", "4"], @cruiser)
+    p @setup.vertical_placement(["A", "B", "C", "D"], ["1", "2", "3", "4"], @cruiser)
+    p @setup.vertical_placement(["A", "B", "C", "D"], ["1", "2", "3", "4"], @submarine)
+    p @setup.vertical_placement(["A", "B", "C", "D"], ["1", "2", "3", "4"], @submarine)
+    p @setup.vertical_placement(["A", "B", "C", "D"], ["1", "2", "3", "4"], @submarine)
+  end
+
+  def test_horizontal
+    puts "\nAll sets should have like letter coordinate"
+    p @setup.horizontal_placement(["A", "B", "C", "D"], ["1", "2", "3", "4"], @cruiser)
+    p @setup.horizontal_placement(["A", "B", "C", "D"], ["1", "2", "3", "4"], @cruiser)
+    p @setup.horizontal_placement(["A", "B", "C", "D"], ["1", "2", "3", "4"], @cruiser)
+    p @setup.horizontal_placement(["A", "B", "C", "D"], ["1", "2", "3", "4"], @submarine)
+    p @setup.horizontal_placement(["A", "B", "C", "D"], ["1", "2", "3", "4"], @submarine)
+    p @setup.horizontal_placement(["A", "B", "C", "D"], ["1", "2", "3", "4"], @submarine)
+  end
+
+  def test_computer_place
   end
 end
